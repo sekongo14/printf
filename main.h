b7ea300f28305b6c1524420cc800cb78bf9ae662
lombre <mboulmbr@gmail.com>
2023-08-21 12:35:21 +0100

    retouche projet

--- main.h
+++ main.h
@@ -25,23 +25,5 @@ typedef struct format
 
 /*Définition de la fonction _printf*/ 
 int _printf(const char *format, ...);
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int rev_string(char *s);
-int printf_char(va_list val);
-int printf_string(const char *s);
-int _putchar(char c);
-int _printf(const char *format, ...);
 
 #endif
