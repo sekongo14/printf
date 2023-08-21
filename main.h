cfb22a5d9847410deef0e2b67f8a2172f69a655d
lombre <mboulmbr@gmail.com>
2023-08-21 14:23:30 +0100

    Update branch dev

--- main.h
+++ main.h
@@ -7,23 +7,12 @@
 #include <limits.h>
 #include <unistd.h>
 #include <string.h>
-
-
 /**
  * struct format - match the conversion specifiers for printf
  * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
  * @f: type pointer to function for the conversion specifier
  *
  */
-
-typedef struct format
-{
-	char *id;
-	int (*f)();
-} convert_match;
-
-
 /*Définition de la fonction _printf*/ 
 int _printf(const char *format, ...);
-
 #endif
